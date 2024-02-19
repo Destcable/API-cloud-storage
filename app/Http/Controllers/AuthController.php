85572846d0babc0aad9d1c21c152d9a1013a481b
Destcable <artmobpavlov21@gmail.com>
2024-02-19 23:33:14 +0300

    [create]

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function authenticate(Request $request)
+    { 
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()->toArray(),
+            ], 422);
+        }
+    
+        $credentials = $validator->validated();
+    
+        if (Auth::attempt($credentials)) {
+            $token = auth()->user()->createToken('AuthToken')->plainTextToken;
+    
+            return response()->json([
+                'success' => true,
+                'message' => 'Success',
+                'token' => $token,
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login failed',
+            ], 401);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:3|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+            'first_name' => 'required',
+            'last_name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()->toArray(),
+            ], 422);
+        }
+
+        $user = \App\Models\User::create([
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+        ]);
+
+        $token = $user->createToken('AuthToken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Success',
+            'token' => $token,
+        ], 200);
+    }
+}
---
+++ database/migrations/2024_02_19_202322_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->timestamps();
+        });
+
+        DB::table('users')->insert([
+            'email' => 'admin@admin.ru',
+            'password' => Hash::make('Qa1'),
+            'first_name' => 'admin',
+            'last_name' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
